a2aecc0e8ac2eebca6d065a28677eae8eb631b67
thorfinn <cherfi.m.abdelwahab@gmail.com>
2026-04-20 08:54:34 +0100

    style: removing overflow hidden for the logo for smaller screens

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,7 +11,7 @@ const Logo = ({ id, title }: LogoProps) => {
     <button
       id={id}
       className={clsx(
-        "group relative z-10 flex items-center gap-1 cursor-pointer overflow-hidden py-1"
+        "group relative z-10 flex items-center gap-1 cursor-pointer  py-1"
       )}
       style={{
         color: "var(--nav-logo-color, #EBE5D0)" 
